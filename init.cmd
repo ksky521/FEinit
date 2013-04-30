bed9b5d35dade214de10a8483e05500fe28ee5e8
ksky521 <ksky521@gmail.com>
2013-04-30 12:23:48 +0800

    showlogo

--- init.cmd
+++ init.cmd
@@ -1,14 +1,7 @@
 @echo off
-color 03
-REM =====================================
-REM   初始化web项目 0.1 Author Theowang
-REM
-REM =====================================
+
 SETLOCAL ENABLEEXTENSIONS
  
-echo.
-echo 初始化web项目 0.1 Author Theowang
-
 :FEinit
 node cmdfe "%~dpf1"
 goto End
