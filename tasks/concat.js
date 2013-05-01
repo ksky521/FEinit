b095f27e730438139cb48dc00181c2d22104b327
ksky521@gmail.com <ksky521@gmail.com>
2013-05-01 12:14:16 +0800

    添加concat任务

--- tasks/concat.js
+++ tasks/concat.js
@@ -1,21 +1,55 @@
+/**
+ * 合并任务
+ * 
+ */
+
 //系统模块
 var path = require('path');
 var join = path.join;
 var fs = require('fs');
 var util = require('util');
+var exec = require('child_process').exec;
 
 var root = join(__dirname, '../');
-var name = path.basename(__filename, '.js');
+// var name = path.basename(__filename, '.js');
 //非系统模块
 var feTask = require(root + '/lib/feTask');
+var grunt = require('grunt');
+var color = require('colors');
 
-var Task = function() {
-    feTask.call(this, name);
-};
+var Task = function() {};
 util.inherits(Task, feTask);
 
 Task.prototype.start = function() {
-    console.log(this)
+    var that = this;
+    if (this.dist.length === 0) {
+        //执行grunt的concat任务
+        that.note('开始grunt concat任务...'.yellow);
+        var child = exec('cd ' + this.root + ' && grunt concat');
+        child.stderr.on('data', function(data) {
+            that.error(data);
+        });
+        child.stdout.on('data', function(data) {
+            that.note(data);
+        });
+
+    } else {
+        //执行自己的concat任务
+        that.note('开始执行合并任务...'.yellow);
+        var gFile = grunt.file;
+        var content = this.dist.filter(function(filepath) {
+            if (!gFile.exists(filepath)) {
+                that.warn('Source file "' + filepath + '" not found.');
+                return false;
+            } else {
+                return true;
+            }
+        }).map(function(v) {
+            var content = gFile.read(v);
+            return content;
+        }).join('\n');
+        that.note('File "' + that.dest + '" created.');
+    }
 }
 
 
